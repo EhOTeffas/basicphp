92eabf21500e40039d4e8e244e410f571ea3cd93
EhOTeffas <marcosvinicioslima2002@gmail.com>
2024-11-07 10:23:09 -0300

    finishing exercise about time conversor

--- desafios2/desafio12 - conversorSegundos/resposta.php
+++ desafios2/desafio12 - conversorSegundos/resposta.php
@@ -13,7 +13,7 @@
         $secs = $_GET['numero'];
         // $_SERVER['PHP_SELF']
         ?>
-        <form action="#" method="get">
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="numero">Qual é o total de Segundos?</label>
             <input type="number" name="numero" id="numero">
             <input type="submit" value="Calcular">
@@ -24,15 +24,21 @@
             <p>Analisando o valor que você digitou, <strong><?=$secs?> segundos</strong> equivalem a um total de: </p>
             <?php 
                 $semanas = (int)($secs/604800);
-                $dias = (($secs/604800)-((int)($secs/604800)))/86400;
-                var_dump($dias);
+                $dias = (($secs % 604800)/86400);
+                $horas = (($secs % 604800)%86400)/3600;
+                $minutos = ((($secs % 604800)%86400)%3600)/60;
+                $segundos = ((($secs % 604800)%86400)%3600)%60;
+                // var_dump($dias);
+                // var_dump($horas);
+                // var_dump($minutos);
+                // var_dump($segundos);
             ?>
             <ul> 
                 <li>  <?=$semanas?> semanas
-                <li>  {} dias
-                <li>  {} horas
-                <li>  {} minutos
-                <li>  {} segundos
+                <li>  <?=(int)$dias?> dias
+                <li>  <?=(int)$horas?> horas
+                <li>  <?=(int)$minutos?> minutos
+                <li>  <?=(int)$segundos?> segundos
             </ul>
         </section>
     </main>
